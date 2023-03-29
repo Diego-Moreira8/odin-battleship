96c137ed05d04bfec0492b91dd7f60c308c88fa4
Diego Moreira <diego.moreira529@gmail.com>
2023-03-29 19:52:15 -0300

    placeShip method tested

--- index.js
+++ index.js
@@ -25,15 +25,49 @@ export class GameBoard {
   }
 
   placeShip(length, x, y, direction) {
+    // Verify the passed arguments
+    if (arguments.length !== 4) {
+      console.log("This method requires 4 arguments");
+      return null;
+    }
+    if (
+      typeof length !== "number" ||
+      typeof x !== "number" ||
+      typeof y !== "number" ||
+      typeof direction !== "string"
+    ) {
+      console.log("One or more of the passed arguments are invalid");
+      return null;
+    }
+    if (length < 0 || length > 4) {
+      console.log("Invalid ship length");
+      return null;
+    }
     if (x > 9 || y > 9) {
       console.log("Invalid coordinates");
       return null;
     }
-    if (direction !== "horizontal" || direction !== "vertical") {
+    if (direction !== "horizontal" && direction !== "vertical") {
       console.log("Invalid direction. Must be: 'horizontal' or 'vertical'");
       return null;
     }
 
     const ship = new Ship(length);
+
+    if (direction === "horizontal") {
+      for (let i = x; i < x + length; i++) {
+        this.board[i][y] = ship;
+      }
+    }
+
+    if (direction === "vertical") {
+      for (let i = y; i < y + length; i++) {
+        this.board[x][i] = ship;
+      }
+    }
+  }
+
+  isOccupied(x, y) {
+    return this.board[x][y] !== undefined ? true : false;
   }
 }
--- index.test.js
+++ index.test.js
@@ -11,8 +11,40 @@ test("Ship creation", () => {
   expect(testShip.isSunk()).toBeTruthy();
 });
 
-test("Board Creation", () => {
-  const testBoard = new GameBoard();
-  expect(testBoard.placeShip(1, 10, 10, "horizontal")).toBeNull;
-  expect(testBoard.placeShip(1, 1, 1, "wrong string")).toBeNull;
+describe("Gameboard - ship placement", () => {
+  let testBoard;
+
+  beforeEach(() => {
+    testBoard = new GameBoard();
+  });
+
+  test("Invalid inputs", () => {
+    expect(testBoard.placeShip(1, 10, 10, "horizontal")).toBeNull();
+    expect(testBoard.placeShip(1, 1, 1, "wrong string")).toBeNull();
+    expect(testBoard.placeShip(-1, 0, 0, "horizontal")).toBeNull();
+    expect(testBoard.placeShip(5, 0, 0, "horizontal")).toBeNull();
+    expect(testBoard.placeShip(5, 0, 0, 0, "horizontal")).toBeNull();
+    expect(testBoard.placeShip(5)).toBeNull();
+    expect(testBoard.placeShip("a", 0, 0, "vertical")).toBeNull();
+    expect(testBoard.placeShip(1, "a", 0, "vertical")).toBeNull();
+    expect(testBoard.placeShip(1, 0, "a", "vertical")).toBeNull();
+    expect(testBoard.placeShip(1, 0, 0, 1)).toBeNull();
+  });
+
+  test("Horizontal placement", () => {
+    expect(testBoard.placeShip(2, 1, 1, "horizontal")).not.toBeNull();
+    expect(testBoard.isOccupied(0, 1)).toBeFalsy();
+    expect(testBoard.isOccupied(1, 1)).toBeTruthy();
+    expect(testBoard.isOccupied(2, 1)).toBeTruthy();
+    expect(testBoard.isOccupied(3, 1)).toBeFalsy();
+  });
+
+  test("Vertical placement", () => {
+    expect(testBoard.placeShip(3, 5, 2, "vertical")).not.toBeNull();
+    expect(testBoard.isOccupied(5, 1)).toBeFalsy();
+    expect(testBoard.isOccupied(5, 2)).toBeTruthy();
+    expect(testBoard.isOccupied(5, 3)).toBeTruthy();
+    expect(testBoard.isOccupied(5, 4)).toBeTruthy();
+    expect(testBoard.isOccupied(5, 5)).toBeFalsy();
+  });
 });
