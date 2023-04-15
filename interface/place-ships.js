79b750e391c214960314027abb95912fe4797635
Diego Moreira <diego.moreira529@gmail.com>
2023-04-14 21:47:26 -0300

    Refactoring place-ships module

--- interface/place-ships.js
+++ interface/place-ships.js
@@ -5,6 +5,238 @@ export default function placeShips(player, passedShipAmount) {
   // Clone shipAmount so this function doesn't make changes on the passed object
   let shipAmount = structuredClone(passedShipAmount);
 
+  clearPage();
+
+  // Page Title
+  const title = document.createElement("h2");
+  title.textContent = `${player.getName()}, posicione suas embarcações!`;
+  document.body.appendChild(title);
+
+  renderOptionsDiv(shipAmount);
+  document.body.appendChild(renderBoard());
+  hoverEffect();
+  clickPositionAction(player, shipAmount);
+}
+
+function renderOptionsDiv(shipAmount) {
+  const controlsDiv = document.createElement("div");
+
+  // Delete button
+  const deleteShipBtn = document.createElement("button");
+  deleteShipBtn.setAttribute("type", "button");
+  deleteShipBtn.setAttribute("id", "delete-ship");
+  deleteShipBtn.textContent = "Apagar embarcação";
+  deleteShipBtn.addEventListener("click", () => {
+    deleteShipBtn.classList.toggle("active");
+  });
+  controlsDiv.appendChild(deleteShipBtn);
+
+  // Rotate button
+  const rotateBtn = document.createElement("button");
+  rotateBtn.setAttribute("type", "button");
+  rotateBtn.setAttribute("id", "direction");
+  rotateBtn.setAttribute("data-current-direction", "horizontal");
+  rotateBtn.textContent = "↻ Horizontal";
+  rotateBtn.addEventListener("click", () => {
+    const currDirection = rotateBtn.getAttribute("data-current-direction");
+    if (currDirection === "horizontal") {
+      rotateBtn.textContent = "↻ Vertical";
+      rotateBtn.setAttribute("data-current-direction", "vertical");
+    } else {
+      rotateBtn.textContent = "↻ Horizontal";
+      rotateBtn.setAttribute("data-current-direction", "horizontal");
+    }
+  });
+  controlsDiv.appendChild(rotateBtn);
+
+  // Ships buttons
+  for (let key in shipAmount) {
+    if (shipAmount[key] > 0) {
+      const shipBtn = document.createElement("button");
+      shipBtn.setAttribute("type", "button");
+      shipBtn.setAttribute("class", "ship");
+      switch (key) {
+        case "shipSize1":
+          shipBtn.textContent = `Destróier`;
+          break;
+        case "shipSize2":
+          shipBtn.textContent = `Submarino`;
+          break;
+        case "shipSize3":
+          shipBtn.textContent = `Cruzador`;
+          break;
+        case "shipSize4":
+          shipBtn.textContent = `Couraçado`;
+          break;
+        case "shipSize5":
+          shipBtn.textContent = `Porta-aviões`;
+          break;
+      }
+      shipBtn.setAttribute("key", `${key}`);
+
+      const shipAmountSpan = document.createElement("span");
+      shipAmountSpan.setAttribute("class", "remaining-ships");
+      shipAmountSpan.textContent = shipAmount[key];
+      shipBtn.appendChild(shipAmountSpan);
+
+      shipBtn.addEventListener("click", () => {
+        deleteShipBtn.classList.remove("active");
+
+        controlsDiv
+          .querySelectorAll(".ship")
+          .forEach((btn) => btn.classList.remove("active"));
+        shipBtn.classList.add("active");
+      });
+
+      controlsDiv.appendChild(shipBtn);
+    }
+  }
+  // Adds an "active" class on the first ship button
+  controlsDiv.querySelector(".ship").classList.add("active");
+
+  document.body.appendChild(controlsDiv);
+}
+
+function hoverEffect() {
+  const positions = document.querySelectorAll(".board-position");
+  positions.forEach((position) => {
+    ["mouseenter", "mouseleave"].forEach((event) => {
+      position.addEventListener(event, () => {
+        // Hover to delete
+        const deleteShipBtn = document.querySelector("#delete-ship");
+        if (deleteShipBtn.classList.contains("active")) {
+          position.classList.toggle("delete-hover");
+        }
+
+        // Hover to place ship
+        const currDirection = document
+          .querySelector("#direction")
+          .getAttribute("data-current-direction");
+
+        const currShipLength = parseInt(
+          document.querySelector(".ship.active").getAttribute("key").slice(-1)
+        );
+
+        const currPosition = {
+          x: parseInt(position.getAttribute("x-coord")),
+          y: parseInt(position.getAttribute("y-coord")),
+        };
+
+        if (currDirection === "horizontal") {
+          for (let i = 0; i < currShipLength; i++) {
+            const position = document.querySelector(
+              `[x-coord='${currPosition.x + i}'][y-coord='${currPosition.y}']`
+            );
+            if (position !== null) {
+              if (currShipLength > 1 && currPosition.x + currShipLength > 10) {
+                position.classList.toggle("place-hover-invalid");
+              } else {
+                position.classList.toggle("place-hover");
+              }
+            }
+          }
+        } else if (currDirection === "vertical") {
+          for (let i = 0; i < currShipLength; i++) {
+            const position = document.querySelector(
+              `[x-coord='${currPosition.x}'][y-coord='${currPosition.y + i}']`
+            );
+            if (position !== null) {
+              if (currShipLength > 1 && currPosition.y + currShipLength > 10) {
+                position.classList.toggle("place-hover-invalid");
+              } else {
+                position.classList.toggle("place-hover");
+              }
+            }
+          }
+        }
+      });
+    });
+  });
+}
+
+function clickPositionAction(player, shipAmount) {
+  const positions = document.querySelectorAll(".board-position");
+  positions.forEach((position) => {
+    position.addEventListener("click", () => {
+      // Delete ship
+      const deleteShipBtn = document.querySelector("#delete-ship");
+      if (deleteShipBtn.classList.contains("active")) {
+        return;
+      }
+
+      // Placing ship
+      const currPosition = {
+        x: parseInt(position.getAttribute("x-coord")),
+        y: parseInt(position.getAttribute("y-coord")),
+      };
+
+      const currDirection = document
+        .querySelector("#direction")
+        .getAttribute("data-current-direction");
+
+      const currShipLength = parseInt(
+        document.querySelector(".ship.active").getAttribute("key").slice(-1)
+      );
+
+      if (
+        player
+          .getBoard()
+          .placeShip(
+            currShipLength,
+            currPosition.x,
+            currPosition.y,
+            currDirection
+          ) !== null
+      ) {
+        const currentShipKey = document
+          .querySelector(".ship.active")
+          .getAttribute("key");
+        shipAmount[currentShipKey] -= 1;
+        syncBoard();
+        updateControlsDiv(currentShipKey);
+      }
+    });
+  });
+
+  function syncBoard() {
+    // Reset classes of the positions
+    document
+      .querySelectorAll(".occupied")
+      .forEach((el) => el.classList.remove("occupied"));
+
+    // Apply the "occupied" class
+    for (let y = 0; y <= 9; y++)
+      for (let x = 0; x <= 9; x++)
+        if (player.getBoard().isOccupied(x, y))
+          document
+            .querySelector(`[x-coord='${x}'][y-coord='${y}']`)
+            .classList.add("occupied");
+  }
+
+  function updateControlsDiv(currentShipKey) {
+    // Update ship amounts and disable when hits 0
+    const btn = document.querySelector(".ship.active");
+    btn.classList.remove("active");
+    const remShips = btn.querySelector(".remaining-ships");
+    remShips.textContent = shipAmount[currentShipKey];
+    btn.disabled = shipAmount[currentShipKey] === 0 ? true : false;
+
+    // Update currShip
+    const shipsBtns = document.querySelectorAll(".ship");
+    for (let btn of shipsBtns) {
+      console.log(btn);
+      if (!btn.disabled) {
+        btn.classList.add("active");
+        break;
+      }
+    }
+  }
+}
+
+function placeShipsOld(player, passedShipAmount) {
+  // Clone shipAmount so this function doesn't make changes on the passed object
+  let shipAmount = structuredClone(passedShipAmount);
+
   // Render page ###############################################################
   clearPage();
 
